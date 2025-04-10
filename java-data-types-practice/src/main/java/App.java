cd3c7e73b209017e60214e839ea49c753e40f6ca
olumide-AI <olumidekolawole26@gmail.com>
2025-04-10 13:38:21 -0700

    Lecture on April 9th, these are the exercises we did in the App.Java class

---
+++ java-data-types-practice/src/main/java/App.java
@@ -0,0 +1,84 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class App {
+    public static void main(String[] args) {
+        //vin will be an int
+        int vinNumber = 9999999;
+        System.out.println("the vin number is: " + vinNumber);
+
+        //vehicle make/model is string
+        String vehicleMake = "Ford Explorer";
+        System.out.println("the vehicle make is: " + vehicleMake);
+
+        //vehicle color is string
+        String vehicleColor = "Red";
+        System.out.println("color: " + vehicleColor);
+
+        //bool to vehicle has a towing package
+        boolean hasTowingPackage = true;
+        System.out.println("has towing package: " + hasTowingPackage );
+
+        //odometer will be a long
+        int odometerReading = 300000;
+        System.out.println("odometer is: " + odometerReading);
+
+        //price will be double
+        double price = 52.50;
+        System.out.println("price is:" + price);
+
+        //Quality rating will be char
+        char qualityRating = 'A';
+        System.out.println("Quality Rating: " + qualityRating);
+
+        //phone number will be string since we won't be changing it
+        String phoneNumber = "3077610892";
+        System.out.println("phone number is: " + phoneNumber);
+
+        //ssn is int
+        int socialSecurityNumber = 32099213;
+        System.out.println("ssn is: " + socialSecurityNumber);
+
+        //zip code is int
+        int zipCode = 94578;
+        System.out.println("zipcode: " + zipCode);
+
+        /**
+         * javadoc comments
+         */
+
+        //Bonus question
+        int x1 = 7;
+        int y1 = 8;
+        int answer1 = x1 + y1;
+        System.out.println("answer1: " + answer1);
+
+        int x2 = 10;
+        int y2 = 4;
+        int answer2 = x2 - y2;
+        System.out.println("answer2: " + answer2);
+
+        int x3 = 2;
+        int y3 = 5;
+        double answer3 = x3 * y3;
+        System.out.println("answer3: " + answer3 );
+
+        double x4 = 20;
+        double y4 = 4;
+        double answer4 = x4 / y4;
+        System.out.println("answer4: " + answer4);
+
+        int x5 = 10;
+        int y5 = 3;
+        int answer5 = x5%y5;
+        System.out.println("answer5: " + answer5);
+
+
+
+
+
+
+
+
+
+    }
+}
