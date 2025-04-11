d4dbd7b0816aea64b58301dd5176e09bcdf81976
olumide-AI <olumidekolawole26@gmail.com>
2025-04-11 01:02:49 -0700

    completed Mod3 Exercise 2 - MathApp

---
+++ MathApplication/src/main/java/MathApp.java
@@ -0,0 +1,19 @@
+public class MathApp {
+    public static void main(String[] args) {
+        // 4. Find and display the square root a variable after it is set to 5.0
+        double numFive = 5.0;
+        double sqrtFive = Math.sqrt(numFive);
+        System.out.println("Square root of five is: " + sqrtFive);
+
+        // 5. Find and display the distance between the points (5, 10) and (85, 50)
+        // An example of widening inserting ints into a double
+        int coordX1 = 5;
+        int ccordY1 = 10;
+        int coordX2 = 85;
+        int coordY2 = 50;
+
+        double distance = Math.sqrt(Math.pow(coordX2 - coordX1, 2) +Math.pow(coordY2 - ccordY1, 2));
+        System.out.println("Distance between the points: " + distance);
+
+    }
+}
