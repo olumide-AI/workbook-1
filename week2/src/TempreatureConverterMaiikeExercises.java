5a0f8c13535388d6356b5c532ae97619166c953f
olumide-AI <olumidekolawole26@gmail.com>
2025-04-17 13:44:42 -0700

    add kelvin conversion to code

--- week2/src/TempreatureConverterMaiikeExercises.java
+++ week2/src/TempreatureConverterMaiikeExercises.java
@@ -19,7 +19,8 @@ public class TempreatureConverterMaiikeExercises {
     public static String userPrompt(Scanner scanner){
         System.out.println("Would you like to convert celsius to fahrenheit or fahrenheit to celsius ");
         System.out.println("Input c to convert fahrenheit to celsius and f to convert celsius to fahrenheit");
-        return scanner.nextLine();
+        System.out.println("You can also input kc to convert kevin to celsius and kf to convert kevin to fahrenheit ");
+        return scanner.nextLine().toLowerCase().trim();
     }
     public static double getTemperature(Scanner scanner){
         System.out.println();
@@ -35,8 +36,14 @@ public class TempreatureConverterMaiikeExercises {
         else if (userCorF.equalsIgnoreCase("f")){
             //means convert celsius to fahrenheit
             return (userTemp * (9.0/5.0) + 32 );
-        }
-        else{
+        } else if (userCorF.equalsIgnoreCase("kc")) {
+            //converts kevin to celsius
+            return userTemp - 273.15;
+        } else if (userCorF.equalsIgnoreCase("kf")) {
+            //convert kevin to fahrenheit
+            return (userTemp - (273.15)*1.8+32.0);
+
+        } else {
             System.out.println("can't convert");
         }
         return userTemp;
@@ -48,6 +55,12 @@ public class TempreatureConverterMaiikeExercises {
         }
         else if (userCorF.equalsIgnoreCase("f")){
             System.out.printf("%.1f celsius is %.1f in fahrenheit ", usertemp, convertedTemp);
+        }
+        else if (userCorF.equalsIgnoreCase("kc")){
+            System.out.printf("%.1f kelvin is %.1f in celsius ", usertemp, convertedTemp);
+        } else if (userCorF.equalsIgnoreCase("kf")) {
+            System.out.printf("%.1f kelvin is %.1f in fahrenheit ", usertemp, convertedTemp);
+
         }
     }
 }
