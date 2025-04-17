c94422b6a15721782153c5633d660e79d4d53839
olumide-AI <olumidekolawole26@gmail.com>
2025-04-16 19:32:06 -0700

    finished code for rental car calc

--- java-data-types-practice/src/main/java/RentalCarCalculator.java
+++ java-data-types-practice/src/main/java/RentalCarCalculator.java
@@ -20,6 +20,16 @@ public class RentalCarCalculator {
         //Number of days renting car
         int noOfDays = noOfDaysNeeded(scanner);
 
+        //Age of customer
+        int customerAge = customerAge(scanner);
+
+        //Calculate and show rental cost
+        carRentalCalc(customerAge, tollTag, gpsNeeded,roadsideAssNeeded, noOfDays);
+
+        //Display result
+
+
+
         //scanner close
         scanner.close();
 
@@ -46,6 +56,11 @@ public class RentalCarCalculator {
         System.out.println("Would you like roadside assistance  with your rental. It's an additional $3.95/day (yes/no)");
         return scanner.nextLine();
     }
+    // Ask for their age
+    public static int customerAge(Scanner scanner){
+        System.out.println("How old are you? There's a 30% subcharge if you're under 25 years old");
+        return scanner.nextInt();
+    }
 
     //Ask user of number of days needed
     public static int noOfDaysNeeded(Scanner scanner){
@@ -54,10 +69,42 @@ public class RentalCarCalculator {
     }
 
     //Logic and Calculations
-    public static void carRentalCalc(int driverAge, ){
-        double basicCarRental = 29.99;
-        if driverAge
+    public static void carRentalCalc(int driverAge, String tollTag, String gpsPay, String roadsipePay, int noOfDays ){
+        double baseRate = 29.99;
+        double optionsCost = 0;
+        double underageSubcharge = 0;
+
+        if (tollTag.equalsIgnoreCase("yes")){
+            optionsCost += 3.95 * noOfDays;
+        }
+        if (gpsPay.equalsIgnoreCase("yes")){
+            optionsCost += 2.95 * noOfDays;
+        }
+        if (roadsipePay.equalsIgnoreCase("yes")){
+            optionsCost += 3.95 * noOfDays;
+        }
+
+        //Base cost
+        double basicCarRentalCost = baseRate * noOfDays;
+
+        //underage drivers extra charge
+        if (driverAge < 25){
+            //Apply a 30% subcharge
+            underageSubcharge = basicCarRentalCost * 0.30;
+        }
+        double total = basicCarRentalCost + optionsCost + underageSubcharge;
+
+        displaySummary(basicCarRentalCost, optionsCost, underageSubcharge, total);
+
     }
+    public static void displaySummary(double baseCost, double optionsCost, double surcharge, double total) {
+        System.out.println("\nRental Summary:");
+        System.out.printf("Base rental cost: $%,.2f%n", baseCost);
+        System.out.printf("Options cost: $%,.2f%n", optionsCost);
+        System.out.printf("Underage driver surcharge: $%,.2f%n", surcharge);
+        System.out.printf("Total cost: $%,.2f%n", total);
+    }
+
 
 }
 
