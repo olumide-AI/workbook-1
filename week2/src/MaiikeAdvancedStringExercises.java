a73e55e1925fc70b150962bd10e131ec5c0b1848
olumide-AI <olumidekolawole26@gmail.com>
2025-04-17 09:41:01 -0700

    greet formally problem

--- week2/src/MaiikeAdvancedStringExercises.java
+++ week2/src/MaiikeAdvancedStringExercises.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class MaiikeAdvancedStringExercises {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("what is your name:");
+        String name = scanner.nextLine();
+
+        String greeting = greetFormally(name);
+        System.out.println(greeting);
+        scanner.close();
+    }
+    public static String greetFormally(String userName){
+        return "Hello, Ms./Mr." + userName;
+    }
+
 }
