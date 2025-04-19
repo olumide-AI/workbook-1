48bb82fcaf4af1890b102778ad991366ec4dc2f4
olumide-AI <olumidekolawole26@gmail.com>
2025-04-19 01:42:52 -0700

    Extra exercises on stringbuilder

--- workbook-2/ExtraMiniStringBuilderExerciseOne.java
+++ workbook-2/ExtraMiniStringBuilderExerciseOne.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ExtraMiniStringBuilderExerciseOne {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder nameBuilder = new StringBuilder();
+        System.out.println("What is your first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("What is your last name: ");
+        String lastName = scanner.nextLine();
+
+        nameBuilder.append(firstName).append(" ").append(lastName);
+        System.out.println("Your full name is: " + nameBuilder);
+
+        System.out.println("Reverse: " + reverseWord(firstName));
+    }
+    public static String reverseWord(String word){
+        StringBuilder reverseString = new StringBuilder(word);
+        return reverseString.reverse().toString();
+    }
 }
