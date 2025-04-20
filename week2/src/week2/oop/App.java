582de8cd264b2728999e3188b6bf725978deddf7
olumide-AI <olumidekolawole26@gmail.com>
2025-04-20 00:17:54 -0700

    base class for dog

---
+++ week2/src/week2/oop/App.java
@@ -0,0 +1,4 @@
+package week2.oop;
+
+public class App {
+}
--- week2/src/week2/oop/Dog.java
+++ week2/src/week2/oop/Dog.java
@@ -1,4 +1,16 @@
 package week2.oop;
 
 public class Dog {
+    // Creating fields
+    static int noOfEyes = 2;
+    static String latinName = "Canis lupus familiaris";
+    String name;
+    String color;
+    String breed;
+    int age;
+    int noOfLegs;
+
+    public void bark() {
+        System.out.println(name + " says: Woof! (Breed: " + breed + ")");
+    }
 }
