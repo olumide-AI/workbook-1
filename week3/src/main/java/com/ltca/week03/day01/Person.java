24f7d344bc0ebbf5683b9b3c07cee1910f9f5e3f
olumide-AI <olumidekolawole26@gmail.com>
2025-04-21 09:46:10 -0700

    changed field to private

--- week3/src/main/java/com/ltca/week03/day01/Person.java
+++ week3/src/main/java/com/ltca/week03/day01/Person.java
@@ -1,7 +1,7 @@
 package com.ltca.week03.day01;
 
 public class Person {
-    public String name;
-    public int age;
-    public String favoriteColor;
+    private String name;
+    private int age;
+    private String favoriteColor;
 }
