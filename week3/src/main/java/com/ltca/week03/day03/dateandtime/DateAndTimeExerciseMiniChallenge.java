cfe8d59afb95b473c00fb676b295d2a5cbb06624
olumide-AI <olumidekolawole26@gmail.com>
2025-04-23 14:20:17 -0700

    Template for extra challenges on date and time

---
+++ week3/src/main/java/com/ltca/week03/day03/dateandtime/DateAndTimeExerciseMiniChallenge.java
@@ -0,0 +1,15 @@
+package com.ltca.week03.day03.dateandtime;
+
+import java.util.Scanner;
+
+public class DateAndTimeExerciseMiniChallenge {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+    //ask user for date in string
+    public static void userPrompt(Scanner scanner){
+        System.out.println("When is your event date: ");
+        String input = scanner.nextLine();
+    }
+}
