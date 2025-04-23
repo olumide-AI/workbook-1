17cad8ed05a7f561642d9c87bfe994c075f4f2c3
olumide-AI <olumidekolawole26@gmail.com>
2025-04-23 14:21:52 -0700

    cell phone exercise

--- workbook-2/CellPhone.java
+++ workbook-2/CellPhone.java
@@ -1,4 +1,61 @@
-package PACKAGE_NAME;
-
 public class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    //Constructor no parameter
+    //Use this. to help set the actual fields
+    public CellPhone() {
+        this.serialNumber =0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+    //Getters and setters
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String setOwner(String owner) {
+        this.owner = owner;
+        return owner;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
+
+
--- workbook-2/CellPhoneApplication.java
+++ workbook-2/CellPhoneApplication.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CellPhoneApplication {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        cellPhoneOne( scanner);
+
+    }
+
+    public static void cellPhoneOne(Scanner scanner) {
+        //Prompt user and collect info
+        System.out.println("What is the serial number? EX.2597153");
+        int serialNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What model is the phone? EX. iPhone 15 Pro Max");
+        String model = scanner.nextLine();
+        System.out.println("Who is the carrier? EX. Verizon");
+        String carrier = scanner.nextLine();
+        System.out.println("What is the phone number? EX. 888-555-1234");
+        String phoneNumber = scanner.nextLine();
+        System.out.println("Who is the owner of the phone? EX. Sandra");
+        String owner = scanner.nextLine();
+
+        //Use the setters of your CellPhone object to add the values entered by the user.
+        //Create new cellphone
+        CellPhone iphone = new CellPhone();
+        iphone.setSerialNumber(serialNumber);
+        iphone.setModel(model);
+        iphone.setCarrier(carrier);
+        iphone.setPhoneNumber(phoneNumber);
+        iphone.setOwner(iphone.setOwner(owner));
+
+        //Using the getters of the CellPhone print the properties of the phone
+        // to the screen.
+        System.out.println("Cell Phone Info:");
+        System.out.println("Serial Number: " + iphone.getSerialNumber());
+        System.out.println("Model: " + iphone.getModel());
+        System.out.println("Carrier: " + iphone.getCarrier());
+        System.out.println("Phone Number: " + iphone.getPhoneNumber());
+        System.out.println("Owner: " + iphone.getOwner());
+
+    }
 }
