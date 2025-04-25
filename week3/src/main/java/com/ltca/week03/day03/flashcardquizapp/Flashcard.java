44655fa46ed9ff781c6168641f24caf13d20db3f
olumide-AI <olumidekolawole26@gmail.com>
2025-04-25 00:22:21 -0700

    Solutions to flashcards mini exercises

--- week3/src/main/java/com/ltca/week03/day03/flashcardquizapp/Flashcard.java
+++ week3/src/main/java/com/ltca/week03/day03/flashcardquizapp/Flashcard.java
@@ -1,4 +1,40 @@
 package com.ltca.week03.day03.flashcardquizapp;
 
 public class Flashcard {
+    private String question;
+    private String answer;
+
+    public Flashcard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+    //getters and setters
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean checkAnswer(String userAnswer) {
+        if (getAnswer().equalsIgnoreCase(userAnswer)) {
+            System.out.println("Correct");
+            return true;
+        }
+        else {
+            System.out.println("Incorrect answer");
+            return false;
+        }
+
+    }
 }
--- week3/src/main/java/com/ltca/week03/day03/flashcardquizapp/MainFlashcard.java
+++ week3/src/main/java/com/ltca/week03/day03/flashcardquizapp/MainFlashcard.java
@@ -1,4 +1,40 @@
 package com.ltca.week03.day03.flashcardquizapp;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MainFlashcard {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        //Call flashcard list
+        List<Flashcard> callFlashcards = flashcardList();
+        int correctAnswer = 0;
+        boolean trackAnswer = false;
+        for (int i = 0; i < callFlashcards.size(); i++) {
+            System.out.println(callFlashcards.get(i).getQuestion());
+            String userAnswer = scanner.nextLine();
+            trackAnswer = callFlashcards.get(i).checkAnswer(userAnswer);
+            if (trackAnswer == true){
+                correctAnswer ++;
+            }
+        }
+        //Find the length
+        int totalQuestion = callFlashcards.size();
+        // Find the percentage correct
+        float percentageCorrect = ((float) correctAnswer /totalQuestion)*100;
+        System.out.println("Total questions: " + totalQuestion);
+        System.out.println("Correct answers: " + correctAnswer);
+        System.out.println("Percentage correct: " + percentageCorrect + "%");
+
+
+    }
+    //initalize new flashcards
+    public static List<Flashcard> flashcardList(){
+        List<Flashcard> flashcards = new ArrayList<>();
+        flashcards.add(new Flashcard("What is the largest Ocean", "Pacific"));
+        flashcards.add(new Flashcard("What is the biggest Mountain", "Mount Everest"));
+        flashcards.add(new Flashcard("What is the largest Derset", "Sahara Desert"));
+        return flashcards;
+    }
 }
