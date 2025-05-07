18323c818f268bfc2ee1c21e3604ed651b26ab00
olumide-AI <olumidekolawole26@gmail.com>
2025-05-07 15:35:53 -0700

    second overload method

--- workbook-4/src/main/java/formattingfactory/NameFormatter.java
+++ workbook-4/src/main/java/formattingfactory/NameFormatter.java
@@ -8,7 +8,32 @@ public class NameFormatter {
     }
 
     //Full detail
-    public static String format(String prefix, String firstName, String middleName, String lastName, String suffix )
+    public static String format(String prefix, String firstName, String middleName, String lastName, String suffix ){
+//        return lastName + ", " + prefix + ". " + firstName + " " + middleName + ", " + suffix;
+        StringBuilder name = new StringBuilder();
+        name.append(lastName + ", ");
+
+        if (!prefix.isEmpty()){
+            name.append(prefix + ". ");
+        }
+
+        name.append(firstName);
+
+        if(!middleName.isEmpty()){
+            name.append(" " + middleName);
+        }
+
+        if (!suffix.isEmpty()){
+            name.append(", " + suffix);
+        }
+
+        return name.toString();
+    }
 
     //fULL STRING NEEDING PARSING
+    public static String format(String fullName){
+        //DR. Mel B Johnson, PHD
+        //Firstly split by comma
+        String prefixToLastname = fullName.split(",");
+    }
 }
