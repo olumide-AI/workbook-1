f4786f9d58851fa3518a3fd22578efc06b0d30f9
olumide-AI <olumidekolawole26@gmail.com>
2025-05-08 23:45:52 -0700

    testing check in and check out method for hotel room

--- workbook-4/src/test/java/hoteloperations/RoomTest.java
+++ workbook-4/src/test/java/hoteloperations/RoomTest.java
@@ -8,14 +8,24 @@ import org.junit.jupiter.api.Test;
 
 public class RoomTest {
     @Test
-    public void testRoomCheckOut(){
+    //MethodName_StateUnderTest_ExpectedBehavior
+    public void CheckIn_Room0ccupiedAndDirty_True(){
+        Room roomTest = new Room(1,true,true,100);
+        //Act
+        roomTest.checkIn();
+        //Assertion
+        assertTrue(roomTest.isOccupied());
+        assertTrue(roomTest.isDirty());
+    }
+    @Test
+    //MethodName_StateUnderTest_ExpectedBehavior
+    public void CheckIn_Room0ccupiedAndNotDirty_False(){
         Room roomTest = new Room(1,false,true,100);
         //Act
-        roomTest.checkOut();
+        roomTest.checkIn();
         //Assertion
-        assertFalse(roomTest.isDirty());
-
-
+        assertTrue(roomTest.isOccupied());
+        assertTrue(roomTest.isDirty());
     }
     @Test
     public void testRoomCheckIn(){
