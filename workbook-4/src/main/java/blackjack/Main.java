4183f7e75e1e88a08e88260efe67961232c7530e
olumide-AI <olumidekolawole26@gmail.com>
2025-05-09 14:54:45 -0700

    create player hand

--- workbook-4/src/main/java/blackjack/Main.java
+++ workbook-4/src/main/java/blackjack/Main.java
@@ -1,19 +1,14 @@
 package blackjack;
 
+import java.util.Scanner;
+
 public class Main {
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Card testCard = new Card("Spades", "K");
 
-        // Card is face down, try getting the point value:
-        System.out.println("Face down value: " + testCard.getPointValue());  // Expect: 0
-
-        // Flip it face up:
-        testCard.flip();
-        System.out.println("Face up value: " + testCard.getPointValue());   // Expect: 11
-
-        // Flip it back face down:
-        testCard.flip();
-        System.out.println("Face down again value: " + testCard.getPointValue());  // Expect: 0
-
+    }
+    public void serUpGame(){
+        System.out.println("Welcome to Blackjack");
+        int numberOfPlayers = scanner.nextInt();
     }
 }
---
+++ workbook-4/src/main/java/blackjack/Player.java
@@ -0,0 +1,19 @@
+package blackjack;
+
+public class Player {
+    private String name;
+    private Hand hand;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new Hand();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+}
