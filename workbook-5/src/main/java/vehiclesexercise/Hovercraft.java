6fbce1d362aef7fd6659d7547c0b9001fa746914
olumide-AI <olumidekolawole26@gmail.com>
2025-05-12 11:35:34 -0700

    Inital commit for workbook 5, exercise 1 on vehicle inheritance

---
+++ workbook-5/src/main/java/vehiclesexercise/Hovercraft.java
@@ -0,0 +1,4 @@
+package vehiclesexercise;
+
+public class Hovercraft {
+}
---
+++ workbook-5/src/main/java/vehiclesexercise/Vehicle.java
@@ -0,0 +1,8 @@
+package vehiclesexercise;
+
+public class Vehicle {
+    private String color;
+    private int numberOfPassengers;
+    private int cargoCapacity;
+    private int fuelCapacity;
+}
