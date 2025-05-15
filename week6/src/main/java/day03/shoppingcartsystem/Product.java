1985795f1df8f670d8d001c737fbf41f8a687f15
olumide-AI <olumidekolawole26@gmail.com>
2025-05-15 06:27:42 -0700

    Maaike exercises may 14th

--- week6/src/main/java/day03/shoppingcartsystem/Product.java
+++ week6/src/main/java/day03/shoppingcartsystem/Product.java
@@ -5,4 +5,31 @@ public class Product {
     //Properties
     String name;
     double price;
+
+    //Constructor
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    //Method
+    public double getFinalPrice(){
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
---
+++ week6/src/main/java/day03/shoppingcartsystem/ShoppingCart.java
@@ -0,0 +1,8 @@
+package day03.shoppingcartsystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    List<Product> productList = new ArrayList<>();
+}
