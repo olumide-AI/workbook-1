5ec36aed8a88bb012b64d0d55db832ed5da643da
olumide-AI <olumidekolawole26@gmail.com>
2025-05-20 10:37:41 -0700

    implmeented bank account and main

--- workbook-6/src/main/java/financeapplication/BankAccount.java
+++ workbook-6/src/main/java/financeapplication/BankAccount.java
@@ -1,13 +1,47 @@
 package financeapplication;
 
 public class BankAccount implements Valuable{
-    @Override
-    public double getValue() {
-        return 0;
+    private String accountNumber;
+    private String owner;
+    private double balance;
+
+    public BankAccount(String accountNumber, String owner, double balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
     }
 
-    public double deposit(){
-    return 0;
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public double getValue() {
+        return balance;
+    }
+    //Adds a balance
+    public void deposit(double amount){
+        balance += amount;
     }
 
     public double withdraw(){
