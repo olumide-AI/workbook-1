2ef557675afa0abf5c138e2d606d52dc0fee7d76
olumide-AI <olumidekolawole26@gmail.com>
2025-06-10 07:02:24 -0700

    logging and jdbc setup

---
+++ workbook-8/src/main/java/jdbctest/jdbctest.java
@@ -0,0 +1,33 @@
+package jdbctest;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class jdbctest {
+    public static void main(String[] args) throws SQLException {
+        String userProduct = getUserInput();
+
+        // 1 get a connection
+        // needed: db url, username, password
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "yearup25");
+
+        // 2 run query = create statement and run it
+        Statement statement = connection.createStatement();
+        String query = "SELECT * FROM Products WHERE ProductName LIKE '%" + userProduct + "%'";
+        ResultSet rs = statement.executeQuery(query);
+
+        // 3 process results
+        while(rs.next()) {
+            System.out.println(rs.getString("ProductName") + " - " + rs.getDouble("UnitPrice"));
+        }
+
+        connection.close();
+
+    }
+
+    public static String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What product are you looking for?");
+        return scanner.nextLine();
+    }
+}
